289123d5a3d618c5049cc863e617c9478a4326e4
amilkar_ditech <amilkarhernandez07@gmail.com>
2024-08-19 20:18:52 -0500

    :construction: Add new project React Init

---
+++ react-project/travel-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Login } from './pages/Login'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ react-project/travel-app/src/components/ButtonsLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ButtonsLogin = () => {
+    return (
+        <div className="container-buttons">
+            <h3><span>Travel</span>App</h3>
+            <div className="buttons">
+                <button className="btn-login">Log in</button>
+                <button className="btn-register">Register</button>
+            </div>
+        </div>
+    )
+}
