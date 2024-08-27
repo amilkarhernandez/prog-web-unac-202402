fdf565e76a7266472e0d201dc680b2b3d524c3eb
amilkar_ditech <amilkarhernandez07@gmail.com>
2024-08-26 20:28:31 -0500

    :boom:  Add consume service login

--- react-project/travel-app/src/App.jsx
+++ react-project/travel-app/src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Login } from './pages/Login'
+import { Autentication } from './pages/Autentication'
+import { Dashboard } from './pages/Dashboard'
 
 function App() {
 
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Autentication />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
